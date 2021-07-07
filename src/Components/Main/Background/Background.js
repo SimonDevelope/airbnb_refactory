b532b3f0e5ff405afbc44a4292eab6d8cf84fe44
SimonDevelope <whwnine@gmail.com>
2021-07-08 00:05:46 +0900

    main완성 footer제작중

--- src/Components/Main/Background/Background.js
+++ src/Components/Main/Background/Background.js
@@ -11,8 +11,8 @@ function Background () {
                     ></source>
                     <img
                     className='Background-image-attr'
-                    src='https://a0.muscache.com/im/pictures/0c38042b-8685-4180-8d9b-12a6892ac6d8.jpg?im_q=highq&im_w=720'
-                    alt='https://a0.muscache.com/im/pictures/0c38042b-8685-4180-8d9b-12a6892ac6d8.jpg?im_q=highq&im_w=720'
+                    src='https://a0.muscache.com/im/pictures/57b9f708-bb12-498c-bc33-769f8fc43e63.jpg?im_w=960'
+                    alt='https://a0.muscache.com/im/pictures/57b9f708-bb12-498c-bc33-769f8fc43e63.jpg?im_w=960'
                     ></img>
                 </picture>
             </div>
